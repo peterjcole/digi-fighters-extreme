884b9dbc8cd64b974ed0e08c74fcf2faa5d458fb
Lauren O'Mara <lauren.omara22@yahoo.co.uk>
2019-05-29 15:33:19 +0100

    gathering player names

--- app.rb
+++ app.rb
@@ -3,7 +3,13 @@ require 'sinatra/base'
 class Battle < Sinatra::Base
   
   get '/' do
-    "Testing infrastructure working"
+    erb(:index)
+  end
+
+  post '/' do 
+    @player_one = params[:PlayerOne]
+    @player_two = params[:PlayerTwo]
+    erb(:names)
   end
 
   run! if app_file == $0
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,7 +1,12 @@
 feature 'Battle' do 
-  scenario 'visit homepage' do 
-    visit '/' 
-    expect(page).to have_content "Testing infrastructure working"
+  scenario 'enter player names in form and see names displayed' do 
+    visit '/'
+    within('form') do 
+      fill_in 'PlayerOne', with: 'Thanos'
+      fill_in 'PlayerTwo', with: 'Fat Thor'
+      click_button 'Let\'s play'
+    end
+    expect(page).to have_content "Thanos Fat Thor"
   end 
 end 
 
